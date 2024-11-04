935ba4d8fd01d846f2cba574e9329bc6db2d1df0
Weihua <fch@10-18-196-212.dynapool.wireless.nyu.edu>
2024-11-04 16:35:12 -0500

    Update GPTPrefix8

--- src/project.v
+++ src/project.v
@@ -1,9 +1,3 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
-`default_nettype none
 
 module BigCircle(output G, P, input Gi, Pi, GiPrev, PiPrev);
   wire e;
@@ -25,7 +19,8 @@ module Triangle(output Si, input Pi, CiPrev);
   xor (Si, Pi, CiPrev);
 endmodule
 
-module tt_um_example (
+
+module tt_um_prefix8 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -46,37 +41,34 @@ assign b = uio_in;
 
 Square sq[7:0](g, p, a, b);
 
-wire [14:8] g1, p1;
+wire [15:8] g1, p1;
 BigCircle bc1_8(g1[8], p1[8], g[1], p[1], g[0], p[0]);
-BigCircle bc1_9(g1[9], p1[9], g[2], p[2], g[1], p[1]);
-BigCircle bc1_10(g1[10], p1[10], g[3], p[3], g[2], p[2]);
-BigCircle bc1_11(g1[11], p1[11], g[4], p[4], g[3], p[3]);
-BigCircle bc1_12(g1[12], p1[12], g[5], p[5], g[4], p[4]);
-BigCircle bc1_13(g1[13], p1[13], g[6], p[6], g[5], p[5]);
-BigCircle bc1_14(g1[14], p1[14], g[7], p[7], g[6], p[6]);
+BigCircle bc1_11(g1[11], p1[11], g[5], p[5], g[4], p[4]);
+BigCircle bc1_13(g1[13], p1[13], g[7], p[7], g[6], p[6]);
+BigCircle bc1_15(g1[15], p1[15], g[4], p[4], g[3], p[3]);
 
-wire [20:15] g2, p2;
-BigCircle bc2_15(g2[15], p2[15], g1[9], p1[9], g[0], p[0]);
-BigCircle bc2_16(g2[16], p2[16], g1[10], p1[10], g1[8], p1[8]);
-BigCircle bc2_17(g2[17], p2[17], g1[11], p1[11], g1[9], p1[9]);
-BigCircle bc2_18(g2[18], p2[18], g1[12], p1[12], g1[10], p1[10]);
-BigCircle bc2_19(g2[19], p2[19], g1[13], p1[13], g1[11], p1[11]);
-BigCircle bc2_20(g2[20], p2[20], g1[14], p1[14], g1[12], p1[12]);
+wire [9:9] g2, p2;
+BigCircle bc2_9(g2[9], p2[9], g[2], p[2], g1[8], p1[8]);
 
-wire [24:21] g3, p3;
-BigCircle bc3_21(g3[21], p3[21], g2[17], p2[17], g[0], p[0]);
-BigCircle bc3_22(g3[22], p3[22], g2[18], p2[18], g1[8], p1[8]);
-BigCircle bc3_23(g3[23], p3[23], g2[19], p2[19], g2[15], p2[15]);
-BigCircle bc3_24(g3[24], p3[24], g2[20], p2[20], g2[16], p2[16]);
+wire [16:10] g3, p3;
+BigCircle bc3_10(g3[10], p3[10], g[3], p[3], g2[9], p2[9]);
+BigCircle bc3_16(g3[16], p3[16], g1[15], p1[15], g2[9], p2[9]);
+
+wire [12:12] g4, p4;
+BigCircle bc4_12(g4[12], p4[12], g1[11], p1[11], g3[10], p3[10]);
+
+wire [17:14] g5, p5;
+BigCircle bc5_14(g5[14], p5[14], g1[13], p1[13], g4[12], p4[12]);
+BigCircle bc5_17(g5[17], p5[17], g[6], p[6], g4[12], p4[12]);
 
 SmallCircle sc0(c[0], g[0]);
 SmallCircle sc1(c[1], g1[8]);
-SmallCircle sc2(c[2], g2[15]);
-SmallCircle sc3(c[3], g2[16]);
-SmallCircle sc4(c[4], g3[21]);
-SmallCircle sc5(c[5], g3[22]);
-SmallCircle sc6(c[6], g3[23]);
-SmallCircle sc7(c[7], g3[24]);
+SmallCircle sc2(c[2], g2[9]);
+SmallCircle sc3(c[3], g3[10]);
+SmallCircle sc4(c[4], g3[16]);
+SmallCircle sc5(c[5], g4[12]);
+SmallCircle sc6(c[6], g5[17]);
+SmallCircle sc7(c[7], g5[14]);
 Triangle tr0(sum[0], p[0], cin);
 Triangle tr1(sum[1], p[1], c[0]);
 Triangle tr2(sum[2], p[2], c[1]);
